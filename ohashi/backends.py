593b03bd02dcfd8e964c5de5db4fd7d3ddbe4d70
Bryan Veloso <bryan@revyver.com>
2012-08-08 23:16:40 -0700

    This was the correct property to override...

--- ohashi/backends.py
+++ ohashi/backends.py
@@ -7,7 +7,7 @@ from storages.backends.s3boto import S3BotoStorage
 
 class CachedStaticS3Storage(CachedFilesMixin, S3BotoStorage):
     def __init__(self, *args, **kwargs):
-        self.bucket_name = getattr(settings, 'STATIC_STORAGE_BUCKET_NAME')
+        kwargs['bucket'] = getattr(settings, 'STATIC_STORAGE_BUCKET_NAME')
         super(CachedStaticS3Storage, self).__init__(*args, **kwargs)
 
     def modified_time(self, name):
