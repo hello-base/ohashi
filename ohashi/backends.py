b82306c1789b698eb9246d9591fc59ac43b630fc
Bryan Veloso <bryan@revyver.com>
2012-08-08 22:09:05 -0700

    Didn't need to backport that method.

--- ohashi/backends.py
+++ ohashi/backends.py
@@ -10,11 +10,6 @@ class CachedStaticS3Storage(CachedFilesMixin, S3BotoStorage):
         self.bucket_name = getattr(settings, 'STATIC_STORAGE_BUCKET_NAME')
         super(CachedStaticS3Storage, self).__init__(*args, **kwargs)
 
-    def save(self, name, content):
-        name = super(CachedStaticS3Storage, self).save(name, content)
-        self.local_storage._save(name, content)
-        return name
-
     def modified_time(self, name):
         name = self._normalize_name(self._clean_name(name))
         entry = self.entries.get(name)
