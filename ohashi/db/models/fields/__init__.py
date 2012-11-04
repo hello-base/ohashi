ef671f77717dfdebc09f792ee6fb3462def08c2b
Bryan Veloso <bryan@github.com>
2012-11-04 13:20:39 -0800

    Splitting out the fields nicely and stuff.

--- ohashi/db/models/fields/__init__.py
+++ ohashi/db/models/fields/__init__.py
@@ -1,11 +1,7 @@
-import psycopg2.extras
-import uuid
-
 from django import forms
 from django.core import exceptions
 from django.db import models, router
-from django.db.models import DateTimeField, Field, ForeignKey
-from django.utils import timezone
+from django.db.models import Field, ForeignKey
 
 from south.modelsinspector import add_introspection_rules
 
@@ -93,38 +89,5 @@ class CustomManagerForeignKey(ForeignKey):
             )
 
 
-# Register this adapter so we can use UUID objects.
-psycopg2.extras.register_uuid()
-
-
-class UUIDField(CharField):
-    """
-    A field which stores a UUID value in hex format. This may also have the
-    Boolean attribute 'auto' which will set the value on initial save to a new
-    UUID value (calculated using the UUID4 method).
-
-    """
-    __metaclass__ = models.SubfieldBase
-
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault('default', uuid.uuid4)
-        kwargs.setdefault('editable', not kwargs.get('primary_key', False))
-        super(UUIDField, self).__init__(*args, **kwargs)
-
-    def db_type(self, connection):
-        return 'uuid'
-
-    def get_db_prep_value(self, value, connection, prepared=False):
-        return self.to_python(value)
-
-    def to_python(self, value):
-        if not value:
-            return None
-        if not isinstance(value, uuid.UUID):
-            value = uuid.UUID(value)
-        return value
-
-
 # South custom field introspection rules.
 add_introspection_rules([], [r'^ohashi\.db\.fields\.CustomManagerForeignKey'])
-add_introspection_rules([([UUIDField], [], {'auto': ['auto', {'default': 'False'}]},)], [r'^ohashi\.db\.fields\.UUIDField'])
