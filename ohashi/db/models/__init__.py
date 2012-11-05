214442c940629d6970d1714161d36925984adee9
Bryan Veloso <bryan@revyver.com>
2012-11-04 23:14:41 -0800

    uuid.py -> uuids.py (Fixing a name clash.)

--- ohashi/db/models/__init__.py
+++ ohashi/db/models/__init__.py
@@ -2,5 +2,5 @@ from django.db.models import *
 
 from ohashi.db.models.fields import *
 from ohashi.db.models.fields.birthdays import BirthdayField
-from ohashi.db.models.fields.uuid import UUIDField
+from ohashi.db.models.fields.uuids import UUIDField
 from ohashi.db.models.managers.birthdays import BirthdayManager
